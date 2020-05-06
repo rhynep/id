efe29d8fa0b23b3cb8734f02774718c4b37befbe
rhynep <64207371+rhynep@users.noreply.github.com>
2020-05-06 16:43:41 -0400

    layout updates
    trying to improve mobile view

--- idjs.js
+++ idjs.js
@@ -1,15 +1,15 @@
 // subliminal symbol flashes 
 subliminal1 = document.getElementById("flashesA"); // gets element by id
-subliminal1.addEventListener("mouseover", onMouseOver1);     // adds event listener so click causes the color to change
+subliminal1.addEventListener("mousemove", onMouseMove);     // adds event listener so click causes the color to change
 
-function OnMouseOver(flashes1) {
+function OnMouseMove(flashes1) {
     flashesA.src = "http://rhynep.github.io/id/images/id_red.png";
     flashesA.style.width = "auto";
 
 }
 
 subliminal1 = document.getElementById("flashesA"); // gets element by id
-subliminal1.addEventListener("mouseout", onMouseOut1);     // adds event listener so click causes the color to change
+subliminal1.addEventListener("mouseout", onMouseOut);     // adds event listener so click causes the color to change
 
 function MouseOut(flashes1) {
     flashesA.src = "";
