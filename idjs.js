7a63be77bf3528de75dea5d1021396567488376d
rhynep <64207371+rhynep@users.noreply.github.com>
2020-05-09 15:17:15 -0400

    updates js and photo sizing

--- idjs.js
+++ idjs.js
@@ -1,3 +1,32 @@
+// javascript photo enlarge
+theSquareDiv1 = document.getElementById("squarediv1");
+theSquare1 = document.getElementById("square1");
+theSquareDiv1.addEventListener("click", expandImage);
+theSquare1 = document.getElementById("square1");
+
+function expandImage() {
+    if (theSquareDiv1.style.position == "") {
+        theSquareDiv1.style.position = "absolute";
+        theSquareDiv1.style.top = "100";
+        theSquareDiv1.style.left = "100";
+        theSquare1.style.width = "auto";
+        theSquare1.src = "https://rhynep.github.io/id/images/square1.png";
+    } 
+    else { // makes it small
+        theSquareDiv1.style.position = "";
+        theSquare1.style.width = "";
+    }
+}
+
+
+
+
+
+
+
+
+
+
 // subliminal symbol flashes 
 
 // red flash
@@ -25,24 +54,4 @@ function MouseOut(flashes1, flashes2) {
 
 
 
-
-// javascript photo enlarge
-theSquareDiv = document.getElementById("images");
-theSquare1 = document.getElementById("square1");
-theSquareDiv.addEventListener("click", expandImage);
-theSquare1 = document.getElementById("square1");
-
-function expandImage() {
-    if (theSquareDiv.style.position == "") {
-        theSquareDiv.style.position = "absolute";
-        theSquareDiv.style.top = "100";
-        theSquareDiv.style.left = "100";
-        theSquare1.style.width = "auto";
-        theSquare1.src = "https://rhynep.github.io/id/images/square1.png";
-    } 
-    else { // makes it small
-        theSquareDiv.style.position = "";
-        theSquare1.style.width = "";
-    }
-}
 
