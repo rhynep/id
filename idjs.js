da61dc142f1ae163d0734c338d89d4b6a8b59c71
rhynep <64207371+rhynep@users.noreply.github.com>
2020-05-10 00:02:13 -0400

    updates

--- idjs.js
+++ idjs.js
@@ -1,10 +1,10 @@
 // javascript photo enlarge
 theSquareDiv1 = document.getElementById("squarediv1");
 theSquare1 = document.getElementById("square1");
-theSquareDiv1.addEventListener("click", expandImage);
+theSquareDiv1.addEventListener("click", expandImage1);
 theSquare1 = document.getElementById("square1");
 
-function expandImage() {
+function expandImage1() {
     if (theSquareDiv1.style.position == "") {
         theSquareDiv1.style.position = "absolute";
         theSquareDiv1.style.top = "100";
@@ -18,6 +18,24 @@ function expandImage() {
     }
 }
 
+theSquareDiv2 = document.getElementById("squarediv2");
+theSquare2 = document.getElementById("square2");
+theSquareDiv2.addEventListener("click", expandImage2);
+theSquare2 = document.getElementById("square2");
+
+function expandImage2() {
+    if (theSquareDiv2.style.position == "") {
+        theSquareDiv2.style.position = "absolute";
+        theSquareDiv2.style.top = "100";
+        theSquareDiv2.style.left = "100";
+        theSquare2.style.width = "auto";
+        theSquare2.src = "https://rhynep.github.io/id/images/square2.png";
+    } 
+    else { // makes it small
+        theSquareDiv2.style.position = "";
+        theSquare2.style.width = "";
+    }
+}
 
 
 
