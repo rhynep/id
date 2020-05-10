e5b9cfe4cd27189c0c36654fbd80d7d0c402cc00
rhynep <64207371+rhynep@users.noreply.github.com>
2020-05-10 01:51:24 -0400

    flowers

--- idjs.js
+++ idjs.js
@@ -1,4 +1,5 @@
 // javascript photo enlarge
+
 theSquareDiv1 = document.getElementById("squarediv1");
 theSquare1 = document.getElementById("square1");
 theSquareDiv1.addEventListener("click", expandImage1);
@@ -39,11 +40,141 @@ function expandImage2() {
 
 
 
+theSquareDiv3 = document.getElementById("squarediv3");
+theSquare3 = document.getElementById("square3");
+theSquareDiv3.addEventListener("click", expandImage3);
+theSquare3 = document.getElementById("square3");
+
+function expandImage3() {
+    if (theSquareDiv3.style.position == "") {
+        theSquareDiv3.style.position = "absolute";
+        theSquareDiv3.style.top = "100";
+        theSquareDiv3.style.left = "100";
+        theSquare3.style.width = "auto";
+        theSquare3.src = "https://rhynep.github.io/id/images/square3.png";
+    } 
+    else { // makes it small
+        theSquareDiv3.style.position = "";
+        theSquare3.style.width = "";
+    }
+}
 
 
+theSquareDiv4 = document.getElementById("squarediv4");
+theSquare4 = document.getElementById("square4");
+theSquareDiv4.addEventListener("click", expandImage4);
+theSquare4 = document.getElementById("square4");
+
+function expandImage4() {
+    if (theSquareDiv4.style.position == "") {
+        theSquareDiv4.style.position = "absolute";
+        theSquareDiv4.style.top = "100";
+        theSquareDiv4.style.left = "100";
+        theSquare4.style.width = "auto";
+        theSquare4.src = "https://rhynep.github.io/id/images/square4.png";
+    } 
+    else { // makes it small
+        theSquareDiv4.style.position = "";
+        theSquare4.style.width = "";
+    }
+}
+
+theSquareDiv5 = document.getElementById("squarediv5");
+theSquare5 = document.getElementById("square5");
+theSquareDiv5.addEventListener("click", expandImage5);
+theSquare5 = document.getElementById("square5");
+
+function expandImage5() {
+    if (theSquareDiv5.style.position == "") {
+        theSquareDiv5.style.position = "absolute";
+        theSquareDiv5.style.top = "100";
+        theSquareDiv5.style.left = "100";
+        theSquare5.style.width = "auto";
+        theSquare5.src = "https://rhynep.github.io/id/images/square5.png";
+    } 
+    else { // makes it small
+        theSquareDiv5.style.position = "";
+        theSquare5.style.width = "";
+    }
+}
+
+theSquareDiv6 = document.getElementById("squarediv6");
+theSquare6 = document.getElementById("square6");
+theSquareDiv6.addEventListener("click", expandImage6);
+theSquare6 = document.getElementById("square6");
+
+function expandImage6() {
+    if (theSquareDiv6.style.position == "") {
+        theSquareDiv6.style.position = "absolute";
+        theSquareDiv6.style.top = "100";
+        theSquareDiv6.style.left = "100";
+        theSquare6.style.width = "auto";
+        theSquare6.src = "https://rhynep.github.io/id/images/square6.png";
+    } 
+    else { // makes it small
+        theSquareDiv6.style.position = "";
+        theSquare6.style.width = "";
+    }
+}
 
 
+theSquareDiv7 = document.getElementById("squarediv7");
+theSquare7 = document.getElementById("square7");
+theSquareDiv7.addEventListener("click", expandImage7);
+theSquare7 = document.getElementById("square7");
 
+function expandImage7() {
+    if (theSquareDiv7.style.position == "") {
+        theSquareDiv7.style.position = "absolute";
+        theSquareDiv7.style.top = "100";
+        theSquareDiv7.style.left = "100";
+        theSquare7.style.width = "auto";
+        theSquare7.src = "https://rhynep.github.io/id/images/square7.png";
+    } 
+    else { // makes it small
+        theSquareDiv7.style.position = "";
+        theSquare7.style.width = "";
+    }
+}
+
+theSquareDiv8 = document.getElementById("squarediv8");
+theSquare8 = document.getElementById("square8");
+theSquareDiv8.addEventListener("click", expandImage8);
+theSquare8 = document.getElementById("square8");
+
+function expandImage8() {
+    if (theSquareDiv8.style.position == "") {
+        theSquareDiv8.style.position = "absolute";
+        theSquareDiv8.style.top = "100";
+        theSquareDiv8.style.left = "100";
+        theSquare8.style.width = "auto";
+        theSquare8.src = "https://rhynep.github.io/id/images/square8.png";
+    } 
+    else { // makes it small
+        theSquareDiv8.style.position = "";
+        theSquare8.style.width = "";
+    }
+}
+
+
+theSquareDiv = document.getElementById("squarediv");
+theSquare = document.getElementById("square");
+theSquareDiv.addEventListener("click", expandImage);
+theSquare = document.getElementById("square");
+
+function expandImage() {
+    if (theSquareDiv.style.position == "") {
+        theSquareDiv.style.position = "absolute";
+        theSquareDiv.style.top = "100";
+        theSquareDiv.style.left = "100";
+        theSquare.style.width = "auto";
+        theSquare.src = "https://rhynep.github.io/id/images/square.png";
+    } 
+    else { // makes it small
+        theSquareDiv.style.position = "";
+        theSquare.style.width = "";
+    }
+}
 
 // subliminal symbol flashes 
 
