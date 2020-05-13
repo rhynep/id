f5f5f4a6537e736d1a639678f9e3888c963c11dd
rhynep <64207371+rhynep@users.noreply.github.com>
2020-05-13 14:48:42 -0400

    phone testing

---
+++ id_subliminal.js
@@ -0,0 +1,23 @@
+// subliminal symbol flashes 
+
+// red flash
+subliminal1 = document.getElementById("flashesA"); 
+subliminal1.addEventListener("mousemove", onMouseMove);    
+
+function OnMouseRollover(flashes1) {
+     flashesA.src = "http://rhynep.github.io/id/images/id_red.png";
+     flashesA.style.width = "auto";
+
+ }
+
+ subliminal1 = document.getElementById("flashesA"); 
+ subliminal1.addEventListener("mouseout", OnMouseOut);     
+  function OnMouseOut(flashes1) {
+     flashesA.src = "";
+ }
+
+
+
+
+
+
